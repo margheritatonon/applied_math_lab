Fixed the numerical intergration with try except statement

kuramoto_numerical.py
@@ -108,9 +108,17 @@ r_solution = brentq(root_find, 0, 1, args=(k, sigma, distr)) #r is between 0 and
 
 #now we do this for all values of k
 theoretical_r_values = []
+print("entering ks loop:")
 for ks in kvalues:
-    r_solution = brentq(root_find, 0, 1, args=(k, sigma, distr)) #r is between 0 and 1, always
-    theoretical_r_values.append(r_solution)
+    print(ks)
+    try:
+        r_solution = brentq(root_find, 0, 1, args=(ks, sigma, distr)) #r is between 0 and 1, always
+        theoretical_r_values.append(r_solution)
+        print(r_solution)
+    except:
+        theoretical_r_values.append(0)
+        print(0)
+    print("---\n")
 
 print(f"r_values = {theoretical_r_values}") #these are always the same!!!!
 
@@ -118,7 +126,6 @@ print(f"r_values = {theoretical_r_values}") #these are always the same!!!!
 #need this because need it for the rinf theoretical computations
 def normal_second_derivative(omega, sigma:float = sigma): 
     return (((omega**2)/(sigma**2)) - 1) * (1/(np.sqrt(2*np.pi*sigma**6))) * np.exp(-(omega**2)/(2*sigma**2))
-
 
 if distr == "cauchy":
     to_plot = []