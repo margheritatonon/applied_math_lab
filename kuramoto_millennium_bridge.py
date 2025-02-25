f9c7527d55fb115b40e56316ad001b3e5c446a82
Margherita <mtonon.ieu2023@student.ie.edu>
2025-02-25 14:15:19 +0100

    Created a function that calculates the maximum r value per plot; then created a plot of maximum r values versus number of pedestrians N

--- kuramoto_millennium_bridge.py
+++ kuramoto_millennium_bridge.py
@@ -31,7 +31,7 @@ def order_parameter_r(theta):
     """
     return np.abs(np.sum(np.exp(1j * theta)) / len(theta))
 
-def bridge_odes(t, x1t, x2t, thetas):
+def bridge_odes(t, x1t, x2t, thetas, omegas = omegas):
     """
     Returns x1_dot, x2_dot, thetas_dot based on x1t, x2t, and thetas.
     """
@@ -60,5 +60,43 @@ ax.plot(time_pts, np.array(r_vals_eulers))
 ax.set_title(f"Order parameter r versus time for n = {n_pedestrians}")
 ax.set_ylabel("r")
 ax.set_xlabel("Time (t)")
-plt.show()
+#plt.show()
 
+
+#creating a graph of the amplitude of the bridge sway with respect to the number of pedestrians driving it
+#N versus amplitude 
+#we should observe a sharp increase of amplitude of bridge oscillatory motion above a critical number of pedestrians
+
+#first approach for amplitude: we just look at the minimum and the maximum
+def calculate_max_amplitude(n, time_pts = time_pts, dt = dt, x1_0 = x1_0, x2_0 = x2_0):
+    """
+    Returns the maximum r value for n pedestrians (as a somewhat measure of amplitude).
+    """
+    omegas = np.random.normal(0, 1, n)
+    thetas_0 = np.random.uniform(0, 2*np.pi, n)
+    initialthetas = thetas_0
+    initialx1, initialx2, initialthetas = x1_0, x2_0, thetas_0
+    r_vals = []
+    for t in time_pts:
+        x1_dot, x2_dot, thetas_dot = bridge_odes(1, initialx1, initialx2, initialthetas, omegas)
+        initialx1 = initialx1 + x1_dot * dt
+        initialx2 = initialx2 + x2_dot * dt
+        initialthetas = initialthetas + thetas_dot * dt
+        initialthetas = np.mod(initialthetas, 2 * np.pi)
+        r_obtained = order_parameter_r(initialthetas)
+        r_vals.append(r_obtained)
+    return np.max(r_vals)
+
+n_pedestrians = np.arange(1, 1002, 10) #from 1 to 500 pedestrians, sampling every 10
+print(n_pedestrians)
+
+max_r_vals = []
+for p in n_pedestrians:
+    max_r_vals.append(calculate_max_amplitude(p))
+
+figur, ax_rs = plt.subplots()
+ax_rs.scatter(n_pedestrians, max_r_vals, label='Amplitude of sway')
+ax_rs.set_title("Maximum r Value Versus Number of Pedestrians", size = 17)
+ax_rs.set_xlabel("Number of Pedestrians (N)", size = 15)
+ax_rs.set_ylabel("Max Amplitude (r)", size = 15)
+plt.show()
